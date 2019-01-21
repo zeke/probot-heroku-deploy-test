b13f819f91beed60b100f324e06588714defaeb8
Zeke Sikelianos <zeke@sikelianos.com>
2019-01-20 21:00:34 -0800

    change response message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = new http.Server()
 
 app.on('request', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.write('Hello World')
+  res.write('Hello World, again')
   res.end('\n')
 })
 
