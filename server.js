7519445ea120ef90b0f8f8f901f6ab15714d911b
Zeke Sikelianos <zeke@sikelianos.com>
2019-01-20 20:52:25 -0800

    basic http server for testing

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const app = new http.Server()
+
+app.on('request', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.write('Hello World')
+  res.end('\n')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`app is listening`)
+})
